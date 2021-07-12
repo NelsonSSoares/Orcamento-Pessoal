76283f2a860346440738d50cec284f3aafd4e1ed
Nelson de Sousa <73802941+NelsonSSoares@users.noreply.github.com>
2021-07-12 12:55:52 -0300

    zerando valores
    zerando vlores dos campos apos gravação

--- js/index.js
+++ js/index.js
@@ -95,6 +95,15 @@ function cadastrarDespesa(){
         document.getElementById('modal_btn').className = 'btn btn-success';
         //JQUERY
         $('#modalRegistraDespesa').modal('show');
+        //recuperando variaveis e zerando valores apos gravar
+        ano.value = ''
+        mes.value = ''
+        dia.value = ''
+        tipo.value = ''
+        descricao.value = ''
+        valor.value = ''
+
+    
     }else{
 
         document.getElementById('modal_titulo').innerHTML = 'Erro na inclusão do registro';
